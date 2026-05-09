3afc04e4fa7eca046f586650a7f0fa856db2baf8
Jahzan Ahamed <jahzanahamed99@gmail.com>
2026-05-09 17:01:07 +0800

    chore: update dependencies and add babel-preset-expo
    - Added babel-preset-expo version ~54.0.10 to package.json and package-lock.json
    - Added react-dom version 19.1.0 to package.json and package-lock.json
    - Added react-native-web version ^0.21.0 to package.json and package-lock.json
    - Updated package-lock.json with new dependencies and their respective versions

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ExpenseProvider } from './src/context/ExpenseContext';
 import RootNavigator from './src/navigation/RootNavigator';
 
 export default function App() {
   return (
-    <ExpenseProvider>
-      <NavigationContainer>
-        <StatusBar style="dark" />
-        <RootNavigator />
-      </NavigationContainer>
-    </ExpenseProvider>
+    <SafeAreaProvider>
+      <ExpenseProvider>
+        <NavigationContainer>
+          <StatusBar style="dark" />
+          <RootNavigator />
+        </NavigationContainer>
+      </ExpenseProvider>
+    </SafeAreaProvider>
   );
 }
